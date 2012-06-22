533095369a73422664c1e11c5814944796603bd3
Marcelo Fraga <marcelofraga89@gmail.com>
2012-06-21 23:54:09 -0300

    Applying functionality to call the slides with control nav

--- javascripts/padawan.slider.js
+++ javascripts/padawan.slider.js
@@ -13,7 +13,9 @@ function PadawanSlider(element) {
   var children = slider.getElementsByTagName('img');
 
   // Add control nav
-  var controlNav = document.createElement('ul');
+  var controlNav = document.createElement('ul'),
+      controlNavLinks;
+
   controlNav.className = 'padawan-control-nav';
   slider.appendChild(controlNav);
 
@@ -32,7 +34,33 @@ function PadawanSlider(element) {
 
     // Populate control nav
     controlNav.innerHTML += '<li><a rel="' + i + '">' + (i + 1) + '</a></li>';
-    controlNav.getElementsByTagName('a')[vars.currentSlide].className = 'active';
+  }
+
+  controlNavLinks = controlNav.getElementsByTagName('a');
+
+  // Set current link active in control nav
+  controlNavLinks[vars.currentSlide].className = 'active';
+
+  for (var i = 0; i < controlNavLinks.length; i++) {
+    var control = controlNavLinks[i];
+
+    // Set event to control nav links
+    control.onclick = function(event) {
+      event.preventDefault();
+
+      // Show slide
+      children[this.rel].style.opacity = 1;
+
+      // Hide current slide
+      children[vars.currentSlide].style.opacity = 0;
+
+      // Update active link in control nav
+      controlNavLinks[this.rel].className = 'active';
+      controlNavLinks[vars.currentSlide].className = '';
+
+      // Update current slide
+      vars.currentSlide = this.rel;
+    };
   }
 
   // Increases total slides
