e72d864f7e7021e9761b95d1fab045be750b93d4
Marcelo Fraga <marcelofraga89@gmail.com>
2012-06-21 23:09:05 -0300

    Adding control nav

--- javascripts/padawan.slider.js
+++ javascripts/padawan.slider.js
@@ -12,6 +12,11 @@ function PadawanSlider(element) {
   // Find slider children
   var children = slider.getElementsByTagName('img');
 
+  // Add control nav
+  var controlNav = document.createElement('ul');
+  controlNav.className = 'padawan-control-nav';
+  slider.appendChild(controlNav);
+
   for (var i = 0; i < children.length; i++) {
     var child = children[i];
 
@@ -19,7 +24,16 @@ function PadawanSlider(element) {
     if (vars.currentSlide == i) {
       child.style.opacity = 1;
     }
-  };
+
+    // Set height to slider
+    if (slider.offsetHeight < child.height) {
+      slider.style.height = child.height + 'px';
+    }
+
+    // Populate control nav
+    controlNav.innerHTML += '<li><a rel="' + i + '">' + (i + 1) + '</a></li>';
+    controlNav.getElementsByTagName('a')[vars.currentSlide].className = 'active';
+  }
 
   // Increases total slides
   vars.totalSlides++;
